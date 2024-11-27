f55006f6abaab15725ab9d4bb8aeab352863c7f3
yuchan <yuchan8003@naver.com>
2024-11-27 20:35:59 +0900

    배포용 코드 추가

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import userController from './controllers/userController.js';
 import groupController from './controllers/groupController.js';
 import ImageController from './controllers/ImageController.js';
 import errorHandler from './middlewares/errorHandler.js';
+import cors from 'cors';
 
 const app = express();
 
@@ -14,16 +15,22 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/group', express.static(path.join(__dirname, 'group')));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use('/profile', express.static(path.join(__dirname, 'profile')));
+//백엔드 코드 배포용 설정
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 
-
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use('/group', express.static(path.join(__dirname, 'group')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/profile', express.static(path.join(__dirname, 'profile')));
 app.use('/api', userController);
 app.use('/api/group', groupController);
+
 
 app.get('/', (req, res) => {
     res.status(201).json('Welcome');
