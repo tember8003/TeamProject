a319b012c3c68bf2f74aee2d80553b4f17d255a9
yuchan <yuchan8003@naver.com>
2024-12-01 20:52:27 +0900

    test

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,8 +1,11 @@
 // authService.js
 import jwt from 'jsonwebtoken';
 
+console.log("JWT_SECRET:", process.env.JWT_SECRET);
+
 function generateToken(user) {
     return jwt.sign({ id: user.id, userNum: user.userNum }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
 }
 
 function verifyToken(token) {
